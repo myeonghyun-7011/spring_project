a10f643ec04da6032a34268d518f524a9f8465a0
myeonghyun-7011 <dhvlftmd7013@naver.com>
2024-03-01 16:54:33 +0900

    46-49

--- src/test/java/com/example/sbb/SbbApplicationTests.java
+++ src/test/java/com/example/sbb/SbbApplicationTests.java
@@ -6,9 +6,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 @SpringBootTest
@@ -66,13 +68,47 @@ class SbbApplicationTests {
         "sbb가 무엇인가요?", "sbb에 대해서 알고 싶습니다.");
     assertEquals(1, q.getId());
   }
-//--------------FindBySubjectLike-----------------------------------------
-@Test
-void testJpa5() {
-  List<Question> qList = this.questionRepository.findBySubjectLike("sbb%");
-  Question q = qList.get(0);
-  assertEquals("sbb가 무엇인가요?", q.getSubject());
-}
 
+  //--------------FindBySubjectLike-----------------------------------------
+  @Test
+  void testJpa5() {
+    List<Question> qList = this.questionRepository.findBySubjectLike("sbb%");
+    Question q = qList.get(0);
+    assertEquals("sbb가 무엇인가요?", q.getSubject());
+  }
+
+  //--------------질문데이터 수정하기-----------------------------------------
+  @Test
+  void testJpa6() {
+    Optional<Question> oq = this.questionRepository.findById(1); // optional 포장용기 라고생각
+    assertTrue(oq.isPresent());
+    Question q = oq.orElse(null); // 데이터가 있으면 들어오고 없으면 null(유연성)
+    q.setSubject("수정된 제목"); // q = 영속객체 Id를 포함하고있음.
+    this.questionRepository.save(q);
+  }
+
+  //--------------질문데이터 삭제하기-----------------------------------------
+  @Test
+  void testJpa7() {
+    assertEquals(2, this.questionRepository.count()); // data 개수 count , 2개만 있기 때문에 2
+    Optional<Question> oq = this.questionRepository.findById(1); // 그중에 1번을 찾고 oq 에 넣어줌
+    assertTrue(oq.isPresent());
+    Question q = oq.get(); // 찾은걸 다시 q에 담아
+    this.questionRepository.delete(q); // 삭제
+    assertEquals(1, this.questionRepository.count()); // 다시 count 하면 1개
+  }
+
+  // Question q = new Quesion();
+  // quesiontRepository.save(q): // Insert
+
+  // Question q = questionRepository.findById(1).get();
+  // q.setSubject("Hi")
+  // quesiontRepository.save(q): // Update
+
+  // Question q = questionRepository.findById(4).get();
+
+  // quesiontRepository.delete(q): // Delete
+
+  //delete from question where id = 4
 
 }
