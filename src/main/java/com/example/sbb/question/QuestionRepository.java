80441c3ab517bbcac03c75ea9ee793ce7accbdd1
myeonghyun-7011 <dhvlftmd7013@naver.com>
2024-03-09 22:19:05 +0900

    폼수정

--- src/main/java/com/example/sbb/question/QuestionRepository.java
+++ src/main/java/com/example/sbb/question/QuestionRepository.java
@@ -1,6 +1,8 @@
 package com.example.sbb.question;
 
 import com.example.sbb.base.RepositoryUtil;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -16,9 +18,12 @@ public interface QuestionRepository extends JpaRepository<Question, Integer>, Re
 
   List<Question> findBySubjectLike(String subject);
 
+
   //--------------truncate를 위한 Foreign key 비활성화,활성화------------------------------------
   @Transactional
   @Modifying
   @Query(value = "ALTER TABLE question AUTO_INCREMENT = 1", nativeQuery = true)
   void truncate();
+
+  Page<Question> findDistinctBySubjectContainsOrContentContainsOrAuthor_usernameContainsOrAnswerList_contentContainsOrAnswerList_author_usernameContains(String kw, String kw1, String kw2, String kw3, String kw4, Pageable pageable);
 }
